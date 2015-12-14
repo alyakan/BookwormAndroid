ec5de29110397532d01e314b4c02e07a37ac7a77
kareemtarek04 <kareem.tarek04@gmail.com>
2015-12-14 02:00:53 +0200

    get for friendlist and newsfeed

--- Bookworm2/app/src/main/java/com/example/andoird/bookworm/FriendList.java
+++ Bookworm2/app/src/main/java/com/example/andoird/bookworm/FriendList.java
@@ -1,23 +1,37 @@
 package com.example.andoird.bookworm;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class FriendList extends AppCompatActivity {
+    private ArrayAdapter<String> friendAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,31 +53,22 @@ public class FriendList extends AppCompatActivity {
 
     protected void populateFriendList(){
         String[] data = {
-                "Aly Yakan",
-                "Menna El-kashef",
-                "Kareem Tarek",
-                "Moustafa Mahmoud",
-                "Rana El-Garem",
-                "Salma Shoukry",
-                "Heba Ashraf",
-                "Mariam Jarkas",
-                "Nadine Samir",
-                "Ahmed Etefy",
-                "Gihan Said"
 
         };
         List<String> friend_ArrayList = new ArrayList<String>(Arrays.asList(data));
-        final ArrayAdapter<String> friendAdapter = new ArrayAdapter<String>(
+       friendAdapter = new ArrayAdapter<String>(
                 this,
                 R.layout.list_item_friend, // The xml component
                 R.id.list_item_friend_text_view, // The textview inside the xml component
                 friend_ArrayList
         );
-        final FriendList friendList = this;
         ListView listview = (ListView) this.findViewById(R.id.listview_friend);
         listview.setAdapter(friendAdapter);
+        new FetchFriendListTask().execute();
+        final FriendList friendList = this;
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+        /*listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String item = friendAdapter.getItem(i);
@@ -73,7 +78,7 @@ public class FriendList extends AppCompatActivity {
                 startActivity(intent);
 
             }
-        });
+        });*/
 
 
     }
@@ -107,6 +112,170 @@ public class FriendList extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public class FetchFriendListTask extends AsyncTask<Void, Void, String[]> {
+        private final String LOG_TAG = FetchFriendListTask.class.getSimpleName();
+
+        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+        @Override
+        protected void onPostExecute(String[] strings) {
+            // Waits for doInBackground to return the array of strings that will update
+            // the adapter
+
+
+            if(strings != null) {
+
+                friendAdapter.clear();
+                friendAdapter.addAll(strings);
+            }
+        }
+
+        @Override
+        protected String[] doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String friendlistJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+
+
+
+                final String FriendList_BASE_URL = "https://bookworm-alyakan.c9users.io/friends.json";
+
+                // final String USER_CREATE_BASE_URL = "https://bookworm-alyakan.c9users.io/users/new";
+                //URL url2 = new URL(USER_CREATE_BASE_URL);
+                // HttpURLConnection urlConnection1 = (HttpURLConnection) url2.openConnection();
+
+                //urlConnection1.setRequestMethod("POST");
+                //               urlConnection.connect();
+//                {"utf8"=>"✓",
+//                        "authenticity_token"=>"RfJIoHs3Ap9z5AJCraU5aa2wmR66vHTbKA3NqhpZb1BjbZHp3JvkZifuHAEcrhhRjYMo1AxSWEjZTSYobhdqYg==",
+//                        "user"=>{"first_name"=>"",
+//                        "last_name"=>"",
+//                        "email"=>"",
+//                        "password"=>"[FILTERED]",
+//                        "password_confirmation"=>"[FILTERED]",
+//                        "gender"=>"",
+//                        "city"=>"",
+//                        "country"=>"",
+//                        "fbtoken"=>""},
+//                    "commit"=>"Create my account"}
+
+                Uri builtUri = Uri.parse(FriendList_BASE_URL).buildUpon()
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                Log.v(LOG_TAG, "Built URI" + url);
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                friendlistJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                        return null;
+                    }
+                }
+            }
+            try{
+                String[] FriendListDetails = getFriendListDataFromJson(friendlistJsonStr);
+                return FriendListDetails;
+            }catch(JSONException e){
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+                return null;
+            }
+
+
+        }
+
+        /*
+        * Take JSON String representing the book list retrieved from the API, convert it
+        * into a JSON Array and extract the information to populate the booklistAdapter
+        * */
+        private String[] getFriendListDataFromJson(String friendlistJsonStr)
+                throws JSONException {
+
+
+            final String OWM_ID = "id";
+            final String OWM_FIRST_NAME = "first_name";
+            final String OWM_EMAIL = "email";
+
+
+
+            JSONArray friendlistArray = new JSONArray(friendlistJsonStr);
+            // bookIDs = new ArrayList<Integer>();
+
+            String[] resultStrs = new String[friendlistArray.length()];
+            for(int i = 0; i < friendlistArray.length(); i++) {
+                String first_name;
+                String email;
+                int ID;
+                try{
+                    Log.v(LOG_TAG, "------------------------------------");
+                    first_name = friendlistArray.getJSONObject(i).getString(OWM_FIRST_NAME);
+                    email = friendlistArray.getJSONObject(i).getString(OWM_EMAIL);
+                    ID = Integer.parseInt(friendlistArray.getJSONObject(i).getString(OWM_ID));
+
+                    resultStrs[i] = first_name;
+
+                    Log.v(LOG_TAG, first_name);
+                    Log.v(LOG_TAG, ID+"");
+                }catch(Exception e){
+                    Log.e(LOG_TAG, e.getMessage());
+                }
+
+            }
+
+
+            return resultStrs;
+
+        }
     }
 
 }
